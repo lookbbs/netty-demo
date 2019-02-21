30574cb061f36f9b2d41f1c5e0142fa78361c95c
yuandongfei <yuandongfei@xiaoniu.com>
2019-02-21 10:14:48 +0800

    优化

--- src/main/java/com/ydf/netty/NettyConfig.java
+++ src/main/java/com/ydf/netty/NettyConfig.java
@@ -1,9 +1,5 @@
 package com.ydf.netty;
 
-import com.ydf.netty.secure.SecureNettyServer;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
